3c7297df5423eeb2441cb94840e5fba53544055f
Baldur Jónsson <baldur@helpdesk.is>
2022-05-03 12:01:03 +0000

    The carousel

--- user/views.py
+++ user/views.py
@@ -5,17 +5,11 @@ from item.models import Item
 
 
 def frontpage(request):
-    context = get_random_items()
+    context = {
+        'items': Item.objects.order_by('?')[0:3]
+    }
     return render(request, 'user/frontpage.html', context)
 
 
-def get_random_items():
-    item_object = list(Item.objects.all().order_by('?'))
-    if len(item_object) > 2:
-        item_object = item_object[0:3]
-    context = {}
-    for i, item in enumerate(item_object):
-        context.update({f'item{i}': item})
-    for i in range(len(context), 3):
-        context.update({f'item{i}': item_object[-1]})
-    return context
+def register(request):
+    pass
