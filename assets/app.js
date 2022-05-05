9072ebf5387bddf7a38f3becdd918fd68bb63ae2
TobyWans <62607977+TobyWans@users.noreply.github.com>
2022-05-05 12:49:49 +0000

    dropdown accordion

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,7 @@
-console.log('asdf')
+// Function for accordion in dropdown.
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.dropdown-accordion').addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+});
+
