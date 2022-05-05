545183a5d70637de51750b00a80255230d978502
FreemanGud <89984333+FreemanGud@users.noreply.github.com>
2022-05-05 15:35:28 +0000

    view fyrir sales og sale

--- item/urls.py
+++ item/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('accept_offer/<int:offer_id>', views.accept_offer, name='accept_offer'),
     path('reject_offer/<int:offer_id>', views.reject_offer, name='reject_offer'),
     path('checkout/<int:offer_id>', views.checkout, name='checkout'),
+    path('get_all_sales', views.get_all_sales, name='get_all_sales'),
+    path('get_sale/<int:sale_id>', views.get_sale, name='get_sale'),
 ]
