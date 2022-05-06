b870e701293e07a6d94ac45259e743923e881e84
Baldur Jónsson <baldur@helpdesk.is>
2022-05-06 15:30:08 +0000

    More checkout

--- item/views/checkout.py
+++ item/views/checkout.py
@@ -6,7 +6,7 @@ from django.utils.timezone import now
 from item.forms import CheckoutContactForm, CheckoutPaymentForm, CheckoutRateForm
 from item.models import Offer, Sale
 from user.forms import ContactForm
-from user.models import Contact, Country
+from user.models import Contact, Country, Rating
 
 CHECKOUT_STEPS = {
     'item:checkout_contact': 'Heimilisfang',
@@ -25,9 +25,20 @@ def __get_steps(request):
             'path': reverse(step_url),
             'name': step_name,
             'active': False,
+            'available': True
         }
         if request.path == step['path']:
             step['active'] = True
+        if step['url'] == 'item:checkout_verify':
+            checkout_session = request.session.get('checkout', False)
+            if not checkout_session:
+                step['available'] = False
+            elif (
+                not checkout_session.get('offer_id')
+                or not checkout_session.get('contact_id')
+                or not checkout_session.get('payment_information')
+            ):
+                step['available'] = False
         steps.append(step)
     return steps
 
@@ -108,7 +119,10 @@ def checkout_rate(request):
         form = CheckoutRateForm(request.POST)
         if form.is_valid():
             checkout_session['rate_information'] = form.cleaned_data
-
+            rating, created = Rating.objects.get_or_create(rater=request.user, rated=offer.item.seller)
+            rating.rating = form.cleaned_data['rating']
+            rating.save()
+            checkout_session['rating_id'] = rating.id
             request.session['checkout'] = checkout_session
             return redirect('item:checkout_verify')
     else:
@@ -124,17 +138,24 @@ def checkout_rate(request):
 
 def checkout_verify(request):
     checkout_session = request.session.get('checkout', False)
+    submittable = True
     if not checkout_session:
         return redirect('item:get_all_offers')
-    offer = Offer.objects.get(pk=checkout_session['offer_id'], accepted=True, user=request.user)
-    contact = Contact.objects.get(pk=checkout_session['contact_id'])
-    if request.method == 'POST':
+    offer = Offer.objects.get(pk=checkout_session.get('offer_id'), accepted=True, user=request.user)
+    try:
+        contact = Contact.objects.get(pk=checkout_session.get('contact_id'))
+    except Contact.DoesNotExist:
+        contact = None
+        submittable = False
+
+    payment_information = checkout_session.get('payment_information')
+    if not payment_information:
+        submittable = False
+    if request.method == 'POST' and submittable:
         sale = Sale()
         sale.fill_from_contact(contact)
         sale.fill_from_offer(offer)
-        payment_information = checkout_session['payment_information']
-        print(payment_information)
-        for field, value in checkout_session['payment_information'].items():
+        for field, value in payment_information.items():
             setattr(sale, field, value)
         sale.sold_at = now()
         sale.save()
@@ -143,9 +164,10 @@ def checkout_verify(request):
         return redirect('item:checkout_thanks')
     context = {
         'offer': offer,
+        'checkout_steps': __get_steps(request),
         'contact': contact,
-        'payment_information': checkout_session['payment_information'],
-        'rate_information': checkout_session['rate_information'],
+        'payment_information': checkout_session.get('payment_information'),
+        'submittable': submittable,
     }
     return render(request, 'item/checkout/verify.html', context)
 
