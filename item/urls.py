ae92a43301eb2de635432185a26af981fb7e53ea
Baldur Jónsson <baldur@helpdesk.is>
2022-05-06 12:28:39 +0000

    Update urls.py

--- item/urls.py
+++ item/urls.py
@@ -12,7 +12,12 @@ urlpatterns = [
     path('get_all_offers', views.get_all_offers, name='get_all_offers'),
     path('accept_offer/<int:offer_id>', views.accept_offer, name='accept_offer'),
     path('reject_offer/<int:offer_id>', views.reject_offer, name='reject_offer'),
-    path('checkout/<int:offer_id>', views.checkout, name='checkout'),
     path('get_all_sales', views.get_all_sales, name='get_all_sales'),
     path('get_sale/<int:sale_id>', views.get_sale, name='get_sale'),
+    path('checkout/<int:offer_id>', views.checkout, name='checkout'),
+    path('checkout/contact', views.checkout_contact, name='checkout_contact'),
+    path('checkout/payment', views.checkout_payment, name='checkout_payment'),
+    path('checkout/rate', views.checkout_rate, name='checkout_rate'),
+    path('checkout/verify', views.checkout_verify, name='checkout_verify'),
+    path('checkout/thanks', views.checkout_thanks, name='checkout_thanks'),
 ]
