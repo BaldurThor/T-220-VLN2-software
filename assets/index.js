1327cd329ff2890c418bb2e100ebb452ef370e5c
TobyWans <62607977+TobyWans@users.noreply.github.com>
2022-05-09 14:24:16 +0000

    profile img crop
    profile image crop and profile layout fix

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,7 @@
 
 import bootstrap from 'bootstrap'
 import bootstrap_icons from 'bootstrap-icons/font/bootstrap-icons.scss'
+import croppie from 'croppie/croppie.js'
 import style from './index.scss'
 import app from './app.js'
 import checkout from './checkout.js'
