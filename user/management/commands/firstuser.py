ea4967a631da5fdd79fa7c21daab4b5b0cf32790
BaldurThor <89985210+BaldurThor@users.noreply.github.com>
2022-05-10 13:13:53 +0000

    Dummy users

--- user/management/commands/firstuser.py
+++ user/management/commands/firstuser.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from django.core.files import File
@@ -6,6 +7,11 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from user.models import UserProfile
 from django.contrib.auth.models import User
+
+
+def getjsonobj(filename):
+    data = open(filename, "r", encoding="utf8")
+    return data
 
 
 class Command(BaseCommand):
@@ -18,3 +24,23 @@ class Command(BaseCommand):
                                    bio='I was born at a very young age')
         with open(os.path.join(settings.BASE_DIR, 'static/images/robot.jpg'), 'rb') as f:
             user_profile.image.save('users/robot.jpg', File(f), save=True)
+
+        data = getjsonobj("dummyusers.json")
+        dataclasses = json.load(data)
+        objects = dataclasses.values()
+        for i in objects:
+            for e in i:
+                username = e['first_name'] + e['last_name'][0]
+                user = User(
+                    username=username,
+                    first_name=e['first_name'],
+                    last_name=e['last_name'],
+                )
+                user.set_password("generic")
+                user.save()
+                user_profile = UserProfile(
+                    user=user,
+                    bio=e['bio']
+                )
+                with open(os.path.join(settings.BASE_DIR, e['image']), 'rb') as f:
+                    user_profile.image.save(f'users/{e["image"].split("/")[-1]}', File(f), save=True)
