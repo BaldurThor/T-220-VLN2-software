3b141ad393619c0eb386d86afeb324f13dec1419
FreemanGud <89984333+FreemanGud@users.noreply.github.com>
2022-05-12 15:40:28 +0000

    popup fyrir accept á offer

--- fire_sale/settings.py
+++ fire_sale/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'user',
     'messaging',
     'item',
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'is'
 
 TIME_ZONE = 'UTC'
 
