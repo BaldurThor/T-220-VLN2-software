e5b534881f8fdf8de60f3c53d98cd6df5d42d075
Baldur Jónsson <baldur@helpdesk.is>
2022-05-12 17:08:03 +0000

    Update settings.py

--- fire_sale/settings.py
+++ fire_sale/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'fire_sale.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates', BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
